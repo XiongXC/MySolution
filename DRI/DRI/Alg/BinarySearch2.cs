55e1a2d5d8608a03eab3b9d9585e113f4f1171ad
XiongXC <xiongxc@yahoo.com>
2013-10-31 17:27:28 -0700

    Recursive BinarySearch

--- DRI/DRI/Alg/BinarySearch2.cs
+++ DRI/DRI/Alg/BinarySearch2.cs
@@ -8,6 +8,14 @@ namespace DRI.Alg
 {
     class BinarySearch2
     {
-        public static void BinarySearch() { }
+        public static bool BinarySearch(int n, int[] a, int startIndex, int endIndex) {
+            bool finded = false;
+            int midIndex = (endIndex-startIndex+2) / 2-1;
+
+            if (n == a[midIndex]) finded = true;
+            if (n > a[midIndex]) finded= BinarySearch(n, a, midIndex + 1, endIndex);
+            else finded = BinarySearch(n, a, startIndex, midIndex - 1);
+            return finded;
+        }       
     }
 }
