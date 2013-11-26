f3f3306ace943fa29124674d7d7c65e86868adc7
XiongXC <xiongxc@yahoo.com>
2013-11-26 15:54:56 -0800

    BinaryTest

--- DRI/DRI/Alg/BinarySearch2.cs
+++ DRI/DRI/Alg/BinarySearch2.cs
@@ -8,18 +8,18 @@ namespace DRI.Alg
 {
     public class BinarySearch2
     {
-        public static bool BinarySearch(int n, int[] a, int startIndex, int endIndex) {
+        public static int BinarySearch(int n, int[] a, int startIndex, int endIndex) {
 
-            bool finded = false;
+            
             if (endIndex >= startIndex)
             {
                 int midIndex = startIndex + (endIndex - startIndex) / 2;
 
-                if (n == a[midIndex]) return finded = true;
-                else if (n > a[midIndex]) return finded = BinarySearch(n, a, midIndex + 1, endIndex);
-                else return finded = BinarySearch(n, a, startIndex, midIndex - 1);                 
+                if (n == a[midIndex]) return midIndex;
+                else if (n > a[midIndex]) return  BinarySearch(n, a, midIndex + 1, endIndex);
+                else return BinarySearch(n, a, startIndex, midIndex - 1);                 
             }
-            else return false;
+            else return -1;
             
         }       
     }
--- DRI/UnitTestDRI/Alg/UnitTest_BinarySearch.cs
+++ DRI/UnitTestDRI/Alg/UnitTest_BinarySearch.cs
@@ -11,7 +11,7 @@ namespace UnitTestDRI.Alg
         [TestMethod]
         public void TestBinarySearch()
         {
-            int[] length = { 0, 1, 2, 23, 68 };
+            int[] length = { 0, 1, 2, 23, 68,101 };
             //int[][] a=new int[length.Length][];
             int key = 547;
             for (int j = 0; j < length.Length; j++)
@@ -22,45 +22,120 @@ namespace UnitTestDRI.Alg
                 for (int i = 0; i < a.Length; i++)
                 {
                     a[i] = new Random().Next(0, 1000);
-                    Thread.Sleep(100);
+                    if (a[i] == key) a[i]++;
+                    Thread.Sleep(10);
                 }
+
+                int matchedIndex =-1;
 
                 if (a.Length == 0)
                 {
-                    bool finded = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
-                    Assert.IsFalse(finded);
+                    matchedIndex = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
+                    Assert.IsFalse(matchedIndex>=0);
                 }
 
                 int keyIndex = a.Length / 3;
+                int temp = 0;
+
                 if (a.Length == 1)
                 {
-                    bool finded = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
-                    Assert.IsFalse(finded);
+                    matchedIndex = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
+                    Assert.IsFalse(matchedIndex>=0);
                 }
                 if (a.Length == 1)
                 {
                     a[keyIndex] = key;
-                    bool finded = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
-                    Assert.IsTrue(finded);
+                    matchedIndex = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
+                    Assert.IsTrue(matchedIndex>=0);
                 }
 
                 if (a.Length > 1)
                 {
+                    temp = a[keyIndex];
                     a[keyIndex] = key;
                     Array.Sort(a);
-                    bool finded = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
-                    Assert.IsTrue(finded);
+                    
+                    matchedIndex = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
+                    Assert.IsTrue(matchedIndex>=0);
                 }
 
                 if (a.Length > 1)
                 {
-                    //a[keyIndex] = key;
-                    Array.Sort(a);
-                    bool finded = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
-                    Assert.IsTrue(finded);
+                    if (matchedIndex >= 0)
+                    {
+                        a[matchedIndex] = temp;
+                        Array.Sort(a);
+                        matchedIndex = BinarySearch2.BinarySearch(key, a, 0, a.Length - 1);
+                        Assert.IsFalse(matchedIndex>=0);
+                    }
                 }
             }
-
         }
+
+        [TestMethod]
+        public void Test_binarySearch()
+        {
+            int[] length = { 0, 1, 2, 23, 68, 101 };
+            //int[][] a=new int[length.Length][];
+            int key = 547;
+            for (int j = 0; j < length.Length; j++)
+            {
+
+                int[] a = new int[length[j]];
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = new Random().Next(0, 1000);
+                    if (a[i] == key) a[i]++;
+                    Thread.Sleep(10);
+                }
+
+                int matchedIndex = -1;
+
+                if (a.Length == 0)
+                {
+                    matchedIndex = BinarySearch.binarySearch(a, key);
+                    Assert.IsFalse(matchedIndex >= 0);
+                }
+
+                int keyIndex = a.Length / 3;
+                int temp = 0;
+
+                if (a.Length == 1)
+                {
+                    matchedIndex = BinarySearch.binarySearch(a,key);
+                    Assert.IsFalse(matchedIndex >= 0);
+                }
+                if (a.Length == 1)
+                {
+                    a[keyIndex] = key;
+                    matchedIndex = BinarySearch.binarySearch(a,key);
+                    Assert.IsTrue(matchedIndex >= 0);
+                }
+
+                if (a.Length > 1)
+                {
+                    temp = a[keyIndex];
+                    a[keyIndex] = key;
+                    Array.Sort(a);
+
+                    matchedIndex = BinarySearch.binarySearch(a, key);
+                    Assert.IsTrue(matchedIndex >= 0);
+                }
+
+                if (a.Length > 1)
+                {
+                    if (matchedIndex >= 0)
+                    {
+                        a[matchedIndex] = temp;
+                        Array.Sort(a);
+                        matchedIndex = BinarySearch.binarySearch(a,key);
+                        Assert.IsFalse(matchedIndex >= 0);
+                    }
+                }
+            }
+        }
+
+
     }
 }
