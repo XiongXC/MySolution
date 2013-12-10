bf578bb2c320fae44abc3496fb0bca5a2ce5288b
XiongXC <xiongxc@yahoo.com>
2013-12-09 22:26:09 -0800

    add 2 files

---
+++ DRI/DRI/Int/Search/FirstItemInSortedArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRI.Int.Search
+{
+    class FirstItemInSortedArray
+    {
+        public int Search(int[] a, int key) {
+            
+            //Use binary Search
+            int i = BinarySearch.Search(a, key);
+            int l = 0;
+
+            if (i >= 0)
+            { 
+                int k=i-1;
+                while (k>=0 && a[i] == a[k]) { k--;}
+                return k++;
+            }
+
+            return i;
+        }
+    }
+}
---
+++ DRI/UnitTestDRI/Alg/UnitTest_Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DRI;
+using DRI.Alg;
+
+namespace UnitTestDRI.Alg
+{
+    [TestClass]
+    public class UnitTest_Stack
+    {
+        [TestMethod]
+        public void TestMethod_Push()
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] a = MyHelper.CreateArray(100);
+
+            for(int i=0;i<a.Length;i++)
+            {
+                stack.Push(a[i]);
+            }
+
+            for(int i= a.Length-1; i>=0; i--)
+            {
+                Assert.AreEqual(a[i],stack.Pop());
+            }
+        }
+    }
+}
