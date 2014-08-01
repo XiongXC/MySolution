af556e6e1555777a17c1f1ac652294d65e90662a
shawn9988 <xiongxc@yahoo.com>
2014-08-01 11:11:07 -0700

    selectionSort

---
+++ DRI/DRI/Alg/Sort/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRI.Alg.Sort
+{
+    public class SelectionSort
+    {
+        public void Sort(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                int min =a[i];
+                int k = -1;
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (min > a[j]) 
+                    { 
+                        min = a[j];
+                        k = j;
+                    }                   
+                }
+
+                a[k] = a[i];
+                a[i] = min;
+
+            }
+        }
+    }
+}
