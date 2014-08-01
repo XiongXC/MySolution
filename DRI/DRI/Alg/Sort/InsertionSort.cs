4e7ad87632a5f44c5a6e8b4b2c4c2c04a3fad390
shawn9988 <xiongxc@yahoo.com>
2014-08-01 11:05:04 -0700

    Add sort folder

---
+++ DRI/DRI/Alg/Sort/InsertionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRI.Alg.Sort
+{
+    public class InsertionSort
+    {
+        public static void Sort(int[] a) 
+        {
+            int j = 0;
+            for (int i = 1; i < a.Length; i++) 
+            {
+                int k = j;
+                int temp=a[i];
+                while (k >= 0 && a[i] < a[k]) k--;
+                if (k != j)
+                {
+                    Shift(a, k+1, j);
+                    a[k + 1] = temp;
+                }
+
+                j++;
+            }
+        }
+
+        static void Shift(int[] a, int i, int j) 
+        {
+            for (int k =j+1; k >i; k--) 
+            {
+                a[k] = a[k - 1];
+            }
+        }
+    }
+}
