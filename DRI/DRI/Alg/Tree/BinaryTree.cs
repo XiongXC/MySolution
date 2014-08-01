c24934cbe21cbcb367b361ac7de7433b39a898a8
XiongXC <xiongxc@yahoo.com>
2014-08-01 07:54:47 -0700

    binaryTree

---
+++ DRI/DRI/Alg/Tree/BinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRI.Alg.Tree
+{
+    public class BinaryTree<T>
+    {
+        T Value;
+        BinaryTree<T> LeftTree;
+        BinaryTree<T> RightTree;
+
+        public BinaryTree(T Value, BinaryTree<T> LeftTree, BinaryTree<T> RightTree)
+        {
+            this.Value = Value;
+            this.LeftTree = LeftTree;
+            this.RightTree = RightTree;
+        }
+
+        public void ZTranverse(BinaryTree<T> BTree)
+        {
+            Queue<T> qi=new Queue<T>();
+
+            
+
+        }
+
+        private Queue<BinaryTree<T>> Tranverse(BinaryTree<T> BTree, int level) 
+        {            
+            Queue<BinaryTree<T>> qb=new Queue<BinaryTree<T>>();
+            if(BTree != null){
+                qb.Enqueue(BTree);
+                if (level % 2 == 0)
+                {
+                        
+
+                }
+            }
+
+            return qb;
+        }
+        
+    }
+}
