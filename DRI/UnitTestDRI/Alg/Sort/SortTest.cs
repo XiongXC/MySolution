102ef21fb48b0730c09fba1e30bd01fc972cb691
shawn9988 <xiongxc@yahoo.com>
2014-08-02 08:27:15 -0700

    Delegate

--- DRI/UnitTestDRI/Alg/Sort/SortTest.cs
+++ DRI/UnitTestDRI/Alg/Sort/SortTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using DRI.Alg.Sort;
 using DRI;
 
@@ -8,29 +9,42 @@ namespace UnitTestDRI.Alg.Sort
     [TestClass]
     public class SortTest
     {
-        [TestMethod]
-        public void InsertSort()
+        LinkedList<int[]> arrList;
+        /// <summary>
+        ///Initialize() is called once during test execution before
+        ///test methods in this test class are executed.
+        ///</summary>
+        [TestInitialize()]
+        public void Initialize()
         {
-            //int length = 50;
-            bool sorted = true;
-            for (int j = 1; j <6; j++)
-            {
-                int length = new Random().Next(0, 1000);
-                int[] a = MyHelper.CreateArray(length);
-                int[] b = new int[length];
+            int arrNo=5;
 
-                for (int i = 0; i < length; i++)
+            for (int i = 0; i < arrNo; i++) 
+            {
+                int arrLength = new Random().Next(0, 100);
+                int[] a = MyHelper.CreateArray(arrLength);
+                arrList.AddLast(a);
+            }          
+        }
+
+        [TestMethod]
+        public void Sort(Delegate sortMethod)
+        {            
+            bool sorted = true;
+            foreach (int[] a in arrList)
+            {               
+                int[] b = new int[a.Length];
+
+                for (int i = 0; i < a.Length; i++)
                 {
                     b[i] = a[i];
                 }
 
                 Array.Sort(b);
 
-                InsertionSort.Sort(a);
+                InsertionSort.Sort(a);                
 
-                
-
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < a.Length; i++)
                 {
                     if (b[i] != a[i]) sorted = false;
                 }
@@ -39,7 +53,14 @@ namespace UnitTestDRI.Alg.Sort
             }
         }
 
-        public delegate 
+        [TestMethod]
+        public void InsertSort()
+        {
+            SortMethod sm=new SortMethod();
+            Sort()
+        }
+
+        public delegate void SortMethod(int[] a);
 
 
     }
