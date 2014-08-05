2b205f78cbf50091ff86fc731470e9b3a9052440
XiongXC <xiongxc@yahoo.com>
2014-08-05 07:08:31 -0700

    CountingSort

---
+++ DRI/DRI/Alg/Sort/CountingSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRI.Alg.Sort
+{
+    public class CountingSort
+    { 
+        public static void Sort(int[] a)
+        {
+            int min = a[0];
+            int max = a[0];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (min > a[i]) min = a[i];
+                if (max < a[i]) max = a[i];
+            }
+
+            for(int i=0; i<a.Length;i++)
+            {
+                a[i] -= min;
+            }
+
+            int[] b = new int[max - min+1];
+
+            for(int j=0;j<a.Length;j++)
+            {
+                b[a[j]]++;
+            }
+
+            for (int k = 1; k < b.Length; k++) 
+            {
+                b[k] += b[k-1];
+            }
+
+            int[] c=new int[a.Length];
+
+            for(int k=0; k<b.Length;k++)
+            {
+                for (int j = 0; j < b[k]; j++)
+                {
+                    c[b[k]+j] = a[k];
+                }
+            }
+
+        }
+    }
+}
